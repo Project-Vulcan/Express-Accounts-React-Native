9d0b08f8dba93bae2896417c56ba2135ba51ed8a
ProjectVulcan <137906658+Project-Vulcan@users.noreply.github.com>
2023-07-12 17:23:58 +0100

    Create styles.js

---
+++ styles.js
@@ -0,0 +1,17 @@
+import color from "color";
+import {StyleSheet, StatusBar} from "react-native"
+
+export const styles = StyleSheet.create({
+
+  homeScreen : { flex: 1, 
+                 justifyContent: "center", 
+                 alignItems: "center", 
+                 backgroundColor: "red"
+                },
+
+  settingsScreen : { flex: 1, 
+                    justifyContent: "center", 
+                    alignItems: "center", 
+                    backgroundColor: "yellow"}
+
+})                                        
