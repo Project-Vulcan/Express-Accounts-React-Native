7eb6369c864c91208d664e9bc8130ecbce8ea97e
apathtea <73314496+apathtea@users.noreply.github.com>
2023-07-31 21:21:19 +0000

    Changes after completing Learning 4

--- jamesapp/App.js
+++ jamesapp/App.js
@@ -1,49 +1,200 @@
-import { Text, View } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
-import { NavigationContainer } from "@react-navigation/native";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { styles } from "./styles"
+import React, {useState} from 'react';
+import {
+  Button,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
-function HomeScreen() {
+const App = () => {
+
+  const flexDirections = ['row', 'row-reverse', 'column', 'column-reverse'];
+  
+  const justifyContents = [
+    'flex-start',
+    'flex-end',
+    'center',
+    'space-between',
+    'space-around',
+    'space-evenly',
+  ];
+  
+  const alignItemsArr = [
+    'flex-start',
+    'flex-end',
+    'center',
+    'stretch',
+    'baseline',
+  ];
+  
+  const wraps = ['nowrap', 'wrap', 'wrap-reverse'];
+  const directions = ['inherit', 'ltr', 'rtl'];
+  const [flexDirection, setFlexDirection] = useState(0);
+  const [justifyContent, setJustifyContent] = useState(0);
+  const [alignItems, setAlignItems] = useState(0);
+  const [direction, setDirection] = useState(0);
+  const [wrap, setWrap] = useState(0);
+
+  const hookedStyles = {
+    flexDirection: flexDirections[flexDirection],
+    justifyContent: justifyContents[justifyContent],
+    alignItems: alignItemsArr[alignItems],
+    direction: directions[direction],
+    flexWrap: wraps[wrap],
+  };
+
+  const changeSetting = (value, options, setterFunction) => {
+    if (value === options.length - 1) {
+      setterFunction(0);
+      return;
+    }
+    setterFunction(value + 1);
+  };
+  
+  const [squares, setSquares] = useState([<Square />, <Square />, <Square />]);
   return (
-    <View style={styles.HomeScreen}>
-      <Text>Home!</Text>
-    </View>
+    <>
+
+
+      <View style={{paddingTop: StatusBar.currentHeight}} />
+
+      {/* ---------- Playing space with boxes ---------- */}
+
+      <View style={[styles.container, styles.playingSpace, hookedStyles]}>
+
+        {squares.map(elem => elem)}
+
+      </View>
+
+      {/* ---------- Playing space ends ---------- */}
+
+
+
+
+      {/* ========== Buttons at bottom for control ========== */}
+
+      <ScrollView style={styles.container}>
+
+        <View style={styles.controlSpace}>
+
+          <View style={styles.buttonView}>
+            <Button
+              title="Change Flex Direction"
+              onPress={() =>
+                changeSetting(flexDirection, flexDirections, setFlexDirection)
+              }
+            />
+            <Text style={styles.text}>{flexDirections[flexDirection]}</Text>
+          </View>
+
+          <View style={styles.buttonView}>
+            <Button
+              title="Change Justify Content"
+              onPress={() =>
+                changeSetting(
+                  justifyContent,
+                  justifyContents,
+                  setJustifyContent,
+                )
+              }
+            />
+            <Text style={styles.text}>{justifyContents[justifyContent]}</Text>
+          </View>
+
+          <View style={styles.buttonView}>
+            <Button
+              title="Change Align Items"
+              onPress={() =>
+                changeSetting(alignItems, alignItemsArr, setAlignItems)
+              }
+            />
+            <Text style={styles.text}>{alignItemsArr[alignItems]}</Text>
+          </View>
+
+          <View style={styles.buttonView}>
+            <Button
+              title="Change Direction"
+              onPress={() => changeSetting(direction, directions, setDirection)}
+            />
+            <Text style={styles.text}>{directions[direction]}</Text>
+          </View>
+
+          <View style={styles.buttonView}>
+            <Button
+              title="Change Flex Wrap"
+              onPress={() => changeSetting(wrap, wraps, setWrap)}
+            />
+            <Text style={styles.text}>{wraps[wrap]}</Text>
+          </View>
+
+          <View style={styles.buttonView}>
+            <Button
+              title="Add Square"
+              onPress={() => setSquares([...squares, <Square />])}
+            />
+          </View>
+
+          <View style={styles.buttonView}>
+            <Button
+              title="Delete Square"
+              onPress={() =>
+                setSquares(squares.filter((v, i) => i !== squares.length - 1))
+              }
+            />
+          </View>
+
+        </View>
+
+      </ScrollView>
+
+      {/* ========== Control buttons ends ========== */}
+
+    </>
   );
-}
+};
 
-function SettingsScreen() {
-  return (
-    <View style={styles.SettingsScreen}>
-      <Text>Settings!</Text>
-    </View>
-  );
-}
+const styles = StyleSheet.create({
 
+  container: {
+    height: '50%',
+  },
 
-const Tab = createBottomTabNavigator();
+  playingSpace: {
+    backgroundColor: 'white',
+    borderColor: 'blue',
+    borderWidth: 3,
+  },
 
-export default function App() {
-  return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen
-          name="Home"
-          component={HomeScreen}
-          options={{ tabBarIcon: makeIconRender("home") }}
-        />
-        <Tab.Screen
-          name="Settings"
-          component={SettingsScreen}
-          options={{ tabBarIcon: makeIconRender("cog") }}
-        />
-      </Tab.Navigator>
-    </NavigationContainer>
-  );
-}
+  controlSpace: {
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    backgroundColor: '#F5F5F5',
+  },
 
-function makeIconRender(name) {
-  return ({ color, size }) => (
-    <MaterialCommunityIcons name={name} color={color} size={size} />
-  );
-}
+  buttonView: {
+    width: '50%',
+    padding: 10,
+  },
+
+  text: {textAlign: 'center'},
+});
+
+const Square = () => (
+  <View
+    style={{
+      width: 100,
+      height: 100,
+      backgroundColor: randomHexColor(),
+    }}
+  />
+);
+
+const randomHexColor = () => {
+  return '#000000'.replace(/0/g, () => {
+    return Math.round(Math.random() * 16).toString(16);
+  });
+};
+
+export default App;
