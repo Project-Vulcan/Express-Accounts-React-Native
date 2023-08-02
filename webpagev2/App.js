2e9c33cab5fc463e43878a2508c0831a1ebef772
apathtea <73314496+apathtea@users.noreply.github.com>
2023-08-02 15:48:17 +0000

    Background change, image is broken

---
+++ webpagev2/App.js
@@ -0,0 +1,16 @@
+import { View, Text } from "react-native";
+
+export default function App() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+        backgroundColor: "black"
+      }}
+    >
+      <img src="caisteclego.jpg" alt="The logo of CaisTec: A red mountain"/>
+    </View>
+  );
+}
